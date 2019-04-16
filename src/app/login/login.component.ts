e3a3c31d7b1af92e52b2cda8581bf91e478a26a1
mramosobinte <mramos@obinte.com>
2019-04-16 11:34:51 -0500

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { LS } from '../constantes/app-constants';
 import { Observable } from 'rxjs/internal/Observable';
 import { AuthService } from '../services/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { UtilService } from '../services/util.service';
 
 @Component({
   selector: 'app-login',
@@ -22,6 +24,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthService,
+    private utilService: UtilService,
     private toastr: ToastrService
   ) { }
 
@@ -29,21 +32,25 @@ export class LoginComponent implements OnInit {
     localStorage.clear();
   }
 
-  ingresar() {
-    if (this.usuario.usuario && this.usuario.contrasena) {
-      this.cargando = true;
-      this.authService.ingresar(this.usuario.usuario, this.usuario.contrasena);
-      this.authService.getIsLogged$().subscribe(respuesta => {
-        this.isLogged = respuesta;
-        if (this.isLogged) {
-          this.router.navigate(["modulos"]);
-        } else {
-          this.cargando = false;
-        }
-      });
+  ingresar(form: NgForm) {
+    this.cargando = true;
+    let formularioTocado = this.utilService.establecerFormularioTocado(form);
+    if (form && form.valid && formularioTocado) {
+      if (this.usuario.usuario && this.usuario.contrasena) {
+        this.authService.ingresar(this.usuario.usuario, this.usuario.contrasena, this);
+      } else {
+        this.toastr.warning(LS.MSJ_USUARIO_CLAVE_NO_INGRESADA, LS.TOAST_ADVERTENCIA);
+        this.cargando = false;
+      }
     } else {
-      this.toastr.warning(LS.MSJ_USUARIO_CLAVE_NO_INGRESADA, LS.TOAST_ADVERTENCIA);
+      this.toastr.error(LS.MSJ_CAMPOS_INVALIDOS, LS.MSJ_TITULO_INVALIDOS);
+      this.cargando = false;
     }
+  }
+
+  logueoCorrecto() {
+    this.router.navigate(["modulos"]).then(() => {
+    });
   }
 
   cambiarFilasTiempo(filas, tiempo) {
